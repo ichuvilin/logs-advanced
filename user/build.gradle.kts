fd70020dde3edd9501861de1aa74af2f5a4885ee
Ivan Chuvilin <ivan.chuvilin@list.ru>
2024-03-16 18:06:13 +0700

    downgrade lib version on user module

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.1.4"
     id("io.freefair.lombok") version "8.6"
 }
-val springCloudVersion by extra("2023.0.0")
+val springCloudVersion by extra("2021.0.2")
 
 group = "com.ichuvilin"
 version = "0.0.1-SNAPSHOT"
@@ -15,10 +15,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("ch.qos.logback:logback-core:1.5.3")
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
-    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("net.logstash.logback:logstash-logback-encoder:5.0")
+    implementation("ch.qos.logback:logback-core")
     implementation("com.github.javafaker:javafaker:1.0.2") {
         exclude(group = "org.yaml", module = "snakeyaml")
     }
--- user/src/main/java/com/ichuvilin/user/UserApplication.java
+++ user/src/main/java/com/ichuvilin/user/UserApplication.java
@@ -3,10 +3,12 @@ package com.ichuvilin.user;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
