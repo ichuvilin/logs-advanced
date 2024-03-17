69450004280024be0fa8f0615cc01dcd0dff9f27
Ivan Chuvilin <ivan.chuvilin@list.ru>
2024-03-17 11:55:12 +0700

    downgrade lib version on company module

--- company/src/main/java/com/ichuvilin/company/CompanyApplication.java
+++ company/src/main/java/com/ichuvilin/company/CompanyApplication.java
@@ -3,10 +3,12 @@ package com.ichuvilin.company;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
 public class CompanyApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
