4588ea3e1b7844d4547c14b28fd5bcb2f6f7234e
Ivan Chuvilin <ivan.chuvilin@list.ru>
2024-03-17 12:28:43 +0700

    added lib for zipkin

--- user/src/main/java/com/ichuvilin/user/service/UserService.java
+++ user/src/main/java/com/ichuvilin/user/service/UserService.java
@@ -6,21 +6,24 @@ import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
-
+    public static final Random RANDOM = new Random();
     private final FakerService fakerService;
 
     public List<String> getUsers() {
         List<String> res = new LinkedList<>();
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        if (RANDOM.nextBoolean()) {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         for (int i = 0; i < 5; i++) {
             res.add(fakerService.getUser());
