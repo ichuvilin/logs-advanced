33bb0697b01f65414a7e99ef4666ef41f6461fdc
Ivan Chuvilin <ivan.chuvilin@list.ru>
2024-03-17 11:58:24 +0700

    downgrade lib version on gateway module

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.3"
+    id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -15,14 +15,12 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2023.0.0"
+extra["springCloudVersion"] = "2021.0.2"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
-    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("net.logstash.logback:logstash-logback-encoder:5.0")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
